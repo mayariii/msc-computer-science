8d087d48453d0e888bcf798a1f5c39e90e558ba4
Nicole Analise Cox <70478809+nicoleanalisecox@users.noreply.github.com>
2021-10-01 20:04:23 +0100

    Create Week3Exercise5.c

---
+++ 1 - Principles of Programming/Week 3/Week3Exercise5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* 
+ * Complete the function printDate below so that it prints out the data
+ * in a struct Date in the format 
+ * 
+ * DD:MMM:YYYY - e.g. 01:JAN:2019
+ */
+
+struct Date
+{
+	int day;
+	char month[4];
+	int year;
+};
+
+void printDate(struct Date d){
+  printf("%02d:%s:%d\n", d.day, d.month, d.year);
+}
+
+int main(void) {
+  struct Date date1;
+  date1.day = 1;
+  date1.month[0] = 'J';
+  date1.month[1] = 'A';
+  date1.month[2] = 'N';
+  date1.month[3] = '\0';
+  date1.year = 2019;
+  printDate(date1);
+
+  return 0;
+}
